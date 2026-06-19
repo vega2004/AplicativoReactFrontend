2ed2febccb7ec79b104b9b8a0ddf995a364f7482
Borre4023 <alexanderdominguez11723@gmail.com>
2026-06-19 00:23:32 -0600

    Todavía seguimos tlabajando, peero hay detalles en el menú de hamburguesas, por ahora, sólo erán que ocupa toda la pantalla.

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const axiosClient = axios.create({
-  baseURL: 'localhost:8081',
+  baseURL: 'http://localhost:8081',
   headers: {
     'Content-Type': 'application/json',
   },
