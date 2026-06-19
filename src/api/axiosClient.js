0192d5106f20ac4a4b3a9ede91ce1b6354ecfd82
Borre4023 <alexanderdominguez11723@gmail.com>
2026-06-18 23:00:10 -0600

    Cambiamos a localhost por ahora

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const axiosClient = axios.create({
-  baseURL: 'http://192.168.1.139:8081',
+  baseURL: 'localhost:8081',
   headers: {
     'Content-Type': 'application/json',
   },
